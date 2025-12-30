71315c69f965cfc4a00e1b0df38818a58d8871b6
MeDous92 <melmougith@gmail.com>
2025-12-30 16:17:04 +0400

    Add Calendly + WhatsApp contact flow

---
+++ src/lib/links.ts
@@ -0,0 +1,5 @@
+export const LINKS = {
+  calendly: "https://calendly.com/melmougith/30min",
+  whatsapp:
+    "https://wa.me/971542185806?text=Hi%20DataPillars%2C%20I%E2%80%99d%20like%20help%20with%20reliable%20reporting%20and%20automation.%20Can%20we%20talk%3F",
+};
